814bb28e84b46b9526a77b5d96940fffd60ae30e
Eugene Samoilov <eugene.samoilov@gmail.com>
2025-12-28 16:36:16 +0900

    checkfile

--- downloader.py
+++ downloader.py
@@ -6,12 +6,19 @@ from pathlib import Path
 site = "https://sakhamusic.ru"
 site_timeout = 3
 start = 1
-limit = 6000
+limit = 10
 download_dir = "sakhamusic/"
 
 def create_dir():
      folder_path = Path(download_dir)
      folder_path.mkdir(parents=True, exist_ok=True)
+
+def check_file(m_name: str):
+    path = Path(m_name)
+    if path.exists():
+        return True
+    else:
+        return False
 
 def download(m_id: int = 1):
     url = f'{site}/download/{m_id}'
@@ -30,17 +37,21 @@ def download(m_id: int = 1):
         raw_name = match.group(1)
         filename = raw_name.encode('latin-1').decode('utf-8')
     filename=download_dir+filename
-    with open(filename, 'wb') as f:
-        try:
-            f.write(response.content)
-            print(f"{m_id} {filename} saved")
-        except (IOError, OSError):
-            print("Error writing to file")
+    if check_file(filename):
+        return
+    else:            
+        with open(filename, 'wb') as f:
+            try:
+                f.write(response.content)
+                print(f"{m_id} {filename} saved")
+            except (IOError, OSError):
+                print("Error writing to file")
 
 def main():
     create_dir()
     for i in range(start,limit+1):
         download(i)
+    print("completed")
 
 if __name__ == "__main__":
         main()
