3f0db018e2101488c53106c373e40c722703a39e
Eugene Samoilov <eugene.samoilov@gmail.com>
2025-12-28 16:05:42 +0900

    dsa

--- downloader.py
+++ downloader.py
@@ -4,55 +4,43 @@ import requests
 from pathlib import Path
 
 site = "https://sakhamusic.ru"
+site_timeout = 3
 start = 1
 limit = 6000
-site_timeout = 3
-dir = "sakhamusic/"
+download_dir = "sakhamusic/"
 
 def create_dir():
-     folder_path = Path(dir)
+     folder_path = Path(download_dir)
      folder_path.mkdir(parents=True, exist_ok=True)
 
-def download():
-    for i in range(start,limit+1):
-        url = f'{site}/download/{i}'
-        try:
-            response = requests.get(url,stream=True,timeout=site_timeout)
-        except requests.exceptions.RequestException:
-            continue
-        cd = response.headers.get("Content-Disposition", "")
-        match = re.search(r'filename="(.+)"', cd)
-        if match:
-            raw_name = match.group(1)
-            filename = raw_name.encode('latin-1').decode('utf-8')
-            print(i,filename)
-        filename=dir+filename
-        print(filename)
-        with open(filename, 'wb') as f:
-            try:
-                f.write(response.content)
-            except (IOError, OSError):
-                print("Error writing to file")
-
-def download2():
-    url = f'{site}/download/9000'
+def download(m_id: int = 1):
+    url = f'{site}/download/{m_id}'
     try:
         response = requests.get(url,stream=True,timeout=site_timeout)
     except requests.exceptions.RequestException:
         print("common exception")
+        return
     if response.headers.get("Content-Length", "") == "4096":
         if response.headers.get("Content-Disposition", "") == 'attachment; filename=" - .mp3"':
             print("pusto")
-
-    # cd = response.headers.get("Content-Disposition", "")
-    # match = re.search(r'filename="(.+)"', cd)
-    # if match:
-    #     raw_name = match.group(1)
-    #     filename = raw_name.encode('latin-1').decode('utf-8')
-    #     print(filename)
+            return
+    cd = response.headers.get("Content-Disposition", "")
+    match = re.search(r'filename="(.+)"', cd)
+    if match:
+        raw_name = match.group(1)
+        filename = raw_name.encode('latin-1').decode('utf-8')
+    filename=download_dir+filename
+    with open(filename, 'wb') as f:
+        try:
+            f.write(response.content)
+            print(f"{m_id} {filename} saved")
+        except (IOError, OSError):
+            print("Error writing to file")
 
 def main():
-    download2()
+    create_dir()
+    for i in range(start,limit+1):
+        download(i)
 
 if __name__ == "__main__":
         main()
